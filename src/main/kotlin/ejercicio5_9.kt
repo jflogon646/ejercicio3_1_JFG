a38a20a6ccaa6711deb7fcdf55c2bcdee224ecbf
jflogon646 <jflor@iesrafaelalberti.es>
2021-10-25 18:13:05 +0200

    5.9 mitad

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -14,5 +14,21 @@ class Cuenta(val numeroCuenta: String,var saldo: Double) {
 }
 
 class Persona(val dni: String){
+    var listadoCuentas: MutableList<String> = mutableListOf<String>()
+    fun añadeCuentas (cuenta: Cuenta): String {
+        var mensaje: String = ""
+        if (listadoCuentas.size < 3) {
+            listadoCuentas.add(cuenta.numeroCuenta)
+            mensaje = "La cuenta ${cuenta.numeroCuenta} ha sido añadida a ${this.dni}. \n "
+        } else {
+            mensaje = "La persona ${this.dni} ha alcanzado el máximo de cuentas. \nNumero de cuentas asignadas: ${listadoCuentas.size}."
+        }
+        return mensaje
+    }
+    fun esMorosa (): String {
+        for (cuenta in listadoCuentas) {
 
+        }
+        return "hola"
+    }
 }
