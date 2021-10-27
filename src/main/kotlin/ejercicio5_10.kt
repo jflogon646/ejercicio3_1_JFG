ecfb1ea89ca6e429c8391e2f712cf8522f448980
jflogon646 <jflor@iesrarfaelalberti.es>
2021-10-27 21:33:11 +0200

    5.10 medio medio

--- src/main/kotlin/ejercicio5_10.kt
+++ src/main/kotlin/ejercicio5_10.kt
@@ -1,5 +1,6 @@
-class Libro(var titulo: String,var autor: String,var NumeroPaginas: Int,cali: Byte) {
+class Libro(var titulo: String, var autor: String, var NumeroPaginas: Int, cali: Byte) {
     var calificacion: Byte
+
     init {
         if (cali > 10) {
             calificacion = 10
@@ -10,12 +11,95 @@ class Libro(var titulo: String,var autor: String,var NumeroPaginas: Int,cali: By
         }
     }
 
+    fun modificaTitulo(cambio: String) {
+        this.titulo = cambio
+    }
+
+    fun modificaAutor(cambio: String) {
+        this.autor = cambio
+    }
+
+    fun modificaPaginas(cambio: Int) {
+        this.NumeroPaginas = cambio
+    }
+
+    fun modificaCali(cambio: Byte) {
+        if (cambio < 0) {
+            this.calificacion = 0
+        } else if (cambio > 10) {
+            this.calificacion = 10
+        } else {
+            this.calificacion = cambio
+        }
+    }
 
     override fun toString(): String {
-        return "Titulo del libro: $titulo\nAutor: $autor\nNúmero de páginas: $NumeroPaginas\nCalificación(0-10): $calificacion"
+        return this.titulo
+        //"Titulo del libro: $titulo\nAutor: $autor\nNúmero de páginas: $NumeroPaginas\nCalificación(0-10): $calificacion"
     }
 
 }
-class ConjuntoLibros() {
+
+class ConjuntoLibros(conjunto: Array<Libro>) {
+    var conjunto10: Array<Libro?> = arrayOfNulls<Libro>(10)
+
+    init {
+        var x = 0 //contador para conjunto de clase
+        var y = 0 //contador auxiliar para casos que tengan mas de 10 libros
+        if (conjunto.size <= 10) {
+            for (libro in conjunto) {
+                conjunto10[x] = libro
+                x++
+            }
+        } else {
+            while (x < 10) {
+                conjunto10[x] = conjunto[y]
+                x++
+                y++
+            }
+        }
+    }
+
+    private fun conjuntoCompleto(): Boolean {
+        var check: Boolean = true
+        var x: Int = 0
+        while (x < 9) {
+            if (conjunto10[x] == null) {
+                check = false
+                x = 10
+            } else {
+                x++
+            }
+        }
+        return check
+    }
+
+    fun insertLibro(libro: Libro) {
+
+        var i: Int = 0
+        var check: Int = 0
+        while (check == 0) {
+            when {
+                i > 9 -> {
+                    check = 1
+                }
+                conjunto10[i] == null -> {
+                    conjunto10[i] = libro
+                    check = 1
+                }
+                conjunto10[i]!!.titulo == libro.titulo -> {
+                    check = 1
+                }
+                conjuntoCompleto() -> {
+                    check = 1
+                }
+                else -> {
+                    i++
+                }
+
+            }
+
+        }
+    }
 
 }
--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -3,8 +3,8 @@
 class Cuenta(val numeroCuenta: String, saldo: Double? = 0.0) {
 
     var saldo = saldo
-    fun consultaSaldo(): String {
-        return "El saldo de la cuenta es ${this.saldo}"
+    fun consultaSaldo(): Double? {
+        return this.saldo
     }
 
     fun recibeAbonos(valor: Double) {
@@ -24,20 +24,20 @@ class Cuenta(val numeroCuenta: String, saldo: Double? = 0.0) {
 class Persona(val dni: String) {
     var listadoCuentas: Array<Cuenta?> = arrayOfNulls<Cuenta>(3)
 
-    fun insertCuenta(cuenta: Cuenta): String {
+    fun insertCuenta(cuenta: Cuenta) {
 
-        var mensaje: String = "La persona ${this.dni} tiene le máximo de cuentas.\n Max: 3."
         var i: Int = 0
         var check: Int = 0
         while (check == 0) {
             when {
+                i > 2 -> {
+                    check = 1
+                }
                 listadoCuentas[i] == null -> {
                     listadoCuentas[i] = cuenta
-                    mensaje = "La cuenta $cuenta ha sido añadida con exito a la posicion $i."
                     check = 1
                 }
                 listadoCuentas[i]!!.numeroCuenta == cuenta.numeroCuenta-> {
-                    mensaje = "La cuenta ${cuenta.numeroCuenta} ya ha sido añadida a la persona ${this.dni}."
                     check = 1
                 }
                 else -> {
@@ -47,23 +47,17 @@ class Persona(val dni: String) {
             }
 
         }
-        return mensaje
     }
 
-    fun esMorosa(): String {
-        var mensaje: String = "La persona ${this.dni} no tiene cuentas en negativo."
+    fun esMorosa(): Boolean {
+        var mensaje: Boolean = false
         for (cuenta in listadoCuentas) {
             if (cuenta != null) {
                 if (cuenta.saldo!! < 0.0) {
-                    mensaje =
-                        "La persona ${this.dni} tiene cuentas en negativo.\nEl saldo de la cuenta ${cuenta.numeroCuenta} es de ${cuenta.saldo}."
+                    mensaje = true
                 }
             }
         }
         return mensaje
-    }
-
-    override fun toString(): String {
-        return "La persona ${this.dni} tiene ${this.listadoCuentas.size}."
     }
 }
